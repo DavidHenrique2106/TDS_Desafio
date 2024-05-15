3be3bf647d045a20d96036c24716b5ff5c23b4aa
David Henrique <dh210605@gmail.com>
2024-05-15 19:26:07 -0300

    feat: final

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -1,82 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faArrowDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUp,
+  faArrowDown,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import { Row } from "antd";
 import styles from "./CityWheather.module.css";
 
-const API_KEY = '9834561cb4f963a25e323173aef3826a';
+const API_KEY = "9834561cb4f963a25e323173aef3826a";
 
 const CityWeather = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [weatherDetail, setWeatherDetail] = useState(null);
   const { cityName } = useParams();
+  const timestampToTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const formattedTime = hours + ":" + minutes.substr(-2);
+    return formattedTime;
+  };
 
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
-        setWeatherData(response.data);
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+        );
+        const fetchedWeatherData = response.data;
+        setWeatherData(fetchedWeatherData);
 
-
-
+        // Verificar se weatherData é definido antes de acessar coord
+        if (fetchedWeatherData && fetchedWeatherData.coord) {
+          const { lat, lon } = fetchedWeatherData.coord;
+          const e = await axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+          );
+          setWeatherDetail(e.data);
+        }
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
     fetchWeather();
   }, [cityName]);
 
-
+  console.log(weatherDetail);
+  console.log(weatherData);
   const getIconUrl = (iconCode) => {
     return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
   };
 
   const determineColor = (description) => {
     switch (description) {
-      case 'clear sky':
-        return '#47B3C6';
-      case 'few clouds':
-      case 'scattered clouds':
-      case 'broken clouds':
-        return '#4E5665';
-      case 'shower rain':
-      case 'rain':
-      case 'thunderstorm':
-        return '#4E5665';
-      case 'snow':
-        return '#C9C9C9';
+      case "clear sky":
+        return "#47B3C6";
+      case "few clouds":
+      case "scattered clouds":
+      case "broken clouds":
+        return "#4E5665";
+      case "shower rain":
+      case "rain":
+      case "thunderstorm":
+        return "#4E5665";
+      case "snow":
+        return "#C9C9C9";
       default:
-        return '#FFFFFF';
+        return "#FFFFFF";
     }
   };
 
   const determineColorFont = (description) => {
     switch (description) {
-      case 'clear sky':
-        return '#FFFFFF';
-      case 'few clouds':
-      case 'scattered clouds':
-      case 'broken clouds':
-        return '#FFFFFF';
-      case 'shower rain':
-      case 'rain':
-      case 'thunderstorm':
-        return '#FFFFFF';
-      case 'snow':
-        return 'black';
+      case "clear sky":
+        return "#FFFFFF";
+      case "few clouds":
+      case "scattered clouds":
+      case "broken clouds":
+        return "#FFFFFF";
+      case "shower rain":
+      case "rain":
+      case "thunderstorm":
+        return "#FFFFFF";
+      case "snow":
+        return "black";
       default:
-        return 'black';
+        return "black";
     }
   };
 
   const contentStyle = {
-    backgroundColor: weatherData ? determineColor(weatherData.weather[0].description) : '#FFFFFF',
-    color: weatherData ? determineColorFont(weatherData.weather[0].description) : '#FFFFFF'
+    backgroundColor: weatherData
+      ? determineColor(weatherData.weather[0].description)
+      : "#FFFFFF",
+    color: weatherData
+      ? determineColorFont(weatherData.weather[0].description)
+      : "#FFFFFF",
   };
 
+  const sunriseTime = timestampToTime(weatherData && weatherData.sys.sunrise);
+  const sunsetTime = timestampToTime(weatherData && weatherData.sys.sunset);
+
+  console.log(sunriseTime, sunsetTime);
   return (
     <>
       <div className={styles.content} style={contentStyle}>
@@ -84,27 +114,52 @@ const CityWeather = () => {
           <>
             <header className={styles.header}>
               <Link to="/">
-                <FontAwesomeIcon icon={faArrowLeft} className={styles.icon} style={contentStyle} />
+                <FontAwesomeIcon
+                  icon={faArrowLeft}
+                  className={styles.icon}
+                  style={contentStyle}
+                />
               </Link>
             </header>
-            <div className={styles['city-weather-container']}>
-              <h1 className={styles['city-name']}>{cityName}</h1>
-              <p className={styles['weather-info']}>{weatherData.weather[0].description}</p>
+            <div className={styles["city-weather-container"]}>
+              <h1 className={styles["city-name"]}>{cityName}</h1>
+              <p className={styles["weather-info"]}>
+                {weatherData.weather[0].description}
+              </p>
               <Row style={{ marginTop: "-90px" }}>
-                <p className={styles['weather-info-temp']}>{weatherData.main.temp}</p>
+                <p className={styles["weather-info-temp"]}>
+                  {weatherData.main.temp}
+                </p>
                 <div className={styles.tempMinMax}>
                   °C
                   <p style={{ marginBottom: "-9px" }}>
-                    <FontAwesomeIcon icon={faArrowUp} style={{ ...contentStyle, marginLeft: "5px", marginRight: "5px" }} />
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      style={{
+                        ...contentStyle,
+                        marginLeft: "5px",
+                        marginRight: "5px",
+                      }}
+                    />
                     {weatherData.main.temp_max}°
                   </p>
                   <p>
-                    <FontAwesomeIcon icon={faArrowDown} style={{ ...contentStyle, marginLeft: "5px", marginRight: "5px" }} />
+                    <FontAwesomeIcon
+                      icon={faArrowDown}
+                      style={{
+                        ...contentStyle,
+                        marginLeft: "5px",
+                        marginRight: "5px",
+                      }}
+                    />
                     {weatherData.main.temp_min}°
                   </p>
                 </div>
               </Row>
-              <div className={styles['weather-icon-item']} style={{ marginTop: "-90px" }}>
+              <div
+                className={styles["weather-icon-item"]}
+                style={{ marginTop: "-90px" }}
+              >
                 <img
                   src={getIconUrl(weatherData.weather[0].icon)}
                   alt={weatherData.weather[0].description}
@@ -113,24 +168,92 @@ const CityWeather = () => {
               </div>
             </div>
 
-            <div className={styles['weather-icons-container']}>
-              <div className={styles['weather-icon-item']}>
+            <div className={styles["weather-icons-container"]}>
+              <div className={styles["weather-icon-item"]}>
                 <p>Dawn</p>
+                {weatherDetail && (
+                  <>
+                    <img
+                      src={getIconUrl(weatherDetail.list[1].weather[0].icon)}
+                      alt={weatherDetail.list[1].weather[0].description}
+                      width={"50px"}
+                    />
+                    <p className={styles.fontList}>
+                      {weatherDetail.list[1].main.temp}°C
+                    </p>
+                  </>
+                )}
               </div>
-              <div className={styles['weather-icon-item']}>
+              <div className={styles["weather-icon-item"]}>
                 <p>Morning</p>
+                {weatherDetail && (
+                  <>
+                    <img
+                      src={getIconUrl(weatherDetail.list[3].weather[0].icon)}
+                      alt={weatherDetail.list[3].weather[0].description}
+                      width={"50px"}
+                    />
+                    <p className={styles.fontList}>
+                      {weatherDetail.list[3].main.temp}°C
+                    </p>
+                  </>
+                )}
               </div>
-              <div className={styles['weather-icon-item']}>
+              <div className={styles["weather-icon-item"]}>
                 <p>Afternoon</p>
+                {weatherDetail && (
+                  <>
+                    <img
+                      src={getIconUrl(weatherDetail.list[5].weather[0].icon)}
+                      alt={weatherDetail.list[5].weather[0].description}
+                      width={"50px"}
+                    />
+                    <p className={styles.fontList}>
+                      {weatherDetail.list[5].main.temp}°C
+                    </p>
+                  </>
+                )}
               </div>
-              <div className={styles['weather-icon-item']}>
+              <div className={styles["weather-icon-item"]}>
                 <p>Night</p>
+                {weatherDetail && (
+                  <>
+                    <img
+                      src={getIconUrl(weatherDetail.list[7].weather[0].icon)}
+                      alt={weatherDetail.list[7].weather[0].description}
+                      width={"50px"}
+                    />
+                    <p className={styles.fontList}>
+                      {weatherDetail.list[7].main.temp}°C
+                    </p>
+                  </>
+                )}
+              </div>
+            </div>
+            <div className={styles.divFooter} style={contentStyle}>
+              <div>
+                <p>Wind Speed</p>
+                {weatherData && weatherData.wind.speed}m/s
+              </div>
+
+              <div>
+                <p>Sunset</p>
+                <p>{sunsetTime}</p>
+              </div>
+
+              <div>
+                <p>Sunrise</p>
+                <p>{sunriseTime}</p>
+              </div>
+
+              <div>
+                <p>humidity</p>
+                {weatherData && weatherData.main.humidity}%
               </div>
             </div>
           </>
         )}
       </div>
-
     </>
   );
 };
