77420715d65cda6c62373918365de0c7a49d2bf7
David Henrique <112733262+DavidHenrique2106@users.noreply.github.com>
2024-05-15 18:36:57 +0000

    chore: pipe 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css'; 
-import earth from './img/earth.jpg'
+import './App.css';
+import earth from './img/globe.svg'
 
 const App = () => {
   return (
     <div className="app-container">
       <h1 className="white-text">Weather</h1>
-      <img src={earth} /> 
-    
+      <img src={earth} width={"150px"} alt="Earth" />
+
       <h2 className="white-text">Select a city</h2>
       <div className="city-container">
         <div className="city-row">
